28bd37e8198cec9e5e4230d70234a64623dc51d0
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-02-06 08:50:03 +0530

    Documented the n1ql query demo.

--- libcb/n1ql_query.cpp
+++ libcb/n1ql_query.cpp
@@ -32,42 +32,57 @@ static void row_callback(lcb_t instance, int callback_type, const lcb_RESPN1QL *
 
 int main()
 {
+    // Couchbase handle instance. Connects to a bucket.
     lcb_t instance = NULL;
     lcb_create_st options;
     lcb_error_t err;
 
+    // Allocate memory for the handle.
     memset(&options, 0, sizeof(options));
     options.version = 3;
     options.v.v3.connstr = "couchbase://localhost";
 
+    // Initialize the handle.
     err = lcb_create(&instance, &options);
     if (err != LCB_SUCCESS)
         end(instance, "unable to create handle", err);
 
+    // Initialize the parameters for connection.
     err = lcb_connect(instance);
     if (err != LCB_SUCCESS)
         end(instance, "unable to connect to server", err);
 
+    // Block till the connection is established.
     lcb_wait(instance);
 
+    // Check if the connection was successful.
     err = lcb_get_bootstrap_status(instance);
     if (err != LCB_SUCCESS)
         end(instance, "unable to get bootstrap status", err);
 
+    // Structure for writing the query.
     lcb_CMDN1QL cmd = {0};
     lcb_N1QLPARAMS *n1ql_params = lcb_n1p_new();
     err = lcb_n1p_setstmtz(n1ql_params, "SELECT * FROM default;");
     if (err != LCB_SUCCESS)
         end(instance, "unable to build query string", err);
 
+    // Build the query.
     lcb_n1p_mkcmd(n1ql_params, &cmd);
+
+    // Set the callback to be invoked for fetching each row.
     cmd.callback = row_callback;
     cmd.host = "localhost";
+
+    // Make the query.
     err = lcb_n1ql_query(instance, NULL, &cmd);
     if (err != LCB_SUCCESS)
         end(instance, "unable to query", err);
 
+    // Reset the query structure for re-use in subsequent queries.
     lcb_n1p_free(n1ql_params);
+
+    // Block till the queries finish.
     lcb_wait(instance);
     lcb_destroy(instance);
 
