c1d8e5d92dd5b99ac3353b231493a28ec26f06d4
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-02-20 11:41:43 +0530

    Reading from stdin.

--- n1ql_js/main.cpp
+++ n1ql_js/main.cpp
@@ -1,17 +1,18 @@
 #include "n1ql_js.h"
-#include "utils/Utils.h"
-//#include "query_exec/QueryEngine.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    string source_code = N1qlUtils::ReadFile(argv[1]);
+    cin >> noskipws;
+    istream_iterator<char> iterator(cin);
+    istream_iterator<char> iterator_end;
+
+    string source_code(iterator, iterator_end);
+
+    cout << "input" << endl << source_code << endl << endl;
 
     ExecJs(source_code);
 
-    /*QueryEngine qEngine;
-    qEngine.ExecQuery("SELECT * FROM default;");
-*/
     return 0;
 }
