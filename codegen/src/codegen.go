1655fd36324d88163e45f926990bcf75adc1a15e
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-02-20 11:48:04 +0530

    Documented code.

--- codegen/src/codegen.go
+++ codegen/src/codegen.go
@@ -8,19 +8,29 @@ import (
 )
 
 func main() {
+	// Input path.
 	input_path := "../n1ql_js/inputs/input_db_test.txt"
+
+	// Read input file.
 	input, err_read := ioutil.ReadFile(input_path)
 	if err_read != nil {
 		panic(err_read)
 	}
 
+	// Binary to execute.
 	cmd := exec.Command("bin/n1ql_js")
+
+	// Set I/O.
 	cmd.Stdin = bytes.NewReader(input)
 	var output bytes.Buffer
 	cmd.Stdout = &output
+
+	// Run the binary.
 	err := cmd.Run()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("output\t", output.String())
+
+	// Output.
+	fmt.Println(output.String())
 }
