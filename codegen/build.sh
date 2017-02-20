34021b48562ccc2dbfab24cf9cdbce7bfef2164e
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-02-20 11:36:50 +0530

    Wrapper successfully calls the binary.

--- codegen/build.sh
+++ codegen/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-cp ../n1ql_js/n1ql_js ./bin/ &&
-./bin/n1ql_js ../n1ql_js/inputs/input_db_test.txt | js-beautify
+#cp ../n1ql_js/n1ql_js ./bin/ &&
+#./bin/n1ql_js ../n1ql_js/inputs/input_db_test.txt | js-beautify
+cp ../n1ql_js/a.out ./bin/ &&
+bin/a.out
--- codegen/src/codegen.go
+++ codegen/src/codegen.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"bytes"
+)
 
 func main() {
-	fmt.Println("hello go")
+	input_path := "../n1ql_js/inputs/input_db_test.txt"
+	input, err_read := ioutil.ReadFile(input_path)
+	if err_read != nil {
+		panic(err_read)
+	}
+
+	cmd := exec.Command("bin/a.out")
+	cmd.Stdin = bytes.NewReader(input)
+	var output bytes.Buffer
+	cmd.Stdout = &output
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("output\t", output.String())
 }
