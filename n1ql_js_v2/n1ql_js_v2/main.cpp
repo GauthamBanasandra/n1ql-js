8ccc20c37e1641112a67ef6a7059866ffb2efaf5
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-03-06 11:00:27 +0530

    Adding v2 of n1ql-js

---
+++ n1ql_js_v2/n1ql_js_v2/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  n1ql_js_v2
+//
+//  Created by Gautham Banasandra on 06/03/17.
+//  Copyright © 2017 Couchbase. All rights reserved.
+//
+
+#include "include/jsify.hpp"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    ifstream file(
+        "/Users/gautham/projects/github/n1ql_js_v2/inputs/input_plain.js");
+    string source((istreambuf_iterator<char>(file)),
+                  istreambuf_iterator<char>());
+    const char *input = source.c_str();
+    string plain_js = parse(input);
+    cout << plain_js << endl;
+
+    return 0;
+}
