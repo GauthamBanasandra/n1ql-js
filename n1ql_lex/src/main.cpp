60b77bc4ee7a62cd12a2bfee8d3392c9e5b2b037
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-03-08 12:25:44 +0530

    Handling single line comments.

--- n1ql_lex/src/main.cpp
+++ n1ql_lex/src/main.cpp
@@ -7,15 +7,11 @@ using namespace std;
 
 int main()
 {
-	ifstream file("inputs/input_select.txt");
+	ifstream file("inputs/input_comment.txt");
 	string source_code((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
 
 	const char* input=source_code.c_str();
 	string plain_js_code;
-	parse(input, &plain_js_code);
-
-	cout << plain_js_code << endl<<endl;
-
 	parse(input, &plain_js_code);
 
 	cout << plain_js_code << endl<<endl;
