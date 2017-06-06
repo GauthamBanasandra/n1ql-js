a8b8fd9f142ab4b141ba567a2b0dd948667b48a8
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-06-06 19:46:04 +0530

    Project struct changes.

---
+++ n1ql_js_v3/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  n1ql_js_v3
+//
+//  Created by Gautham Banasandra on 06/06/17.
+//  Copyright © 2017 Couchbase. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello" << std::endl;
+}
