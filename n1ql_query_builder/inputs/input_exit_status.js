a2ca3759f70c685d5dcdb9bdac5c702b2b29dcf3
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-07-06 12:15:18 +0530

    Add src files from node.

--- n1ql_query_builder/inputs/input_exit_status.js
+++ n1ql_query_builder/inputs/input_exit_status.js
@@ -1,7 +1,5 @@
-callAndPauseOnStart(function () {
-    var list = [1, 2, 3, 4, 5];
-
-    for(var l of list){
-        print(l);
+function query() {
+    for (var r of res) {
+        return r;
     }
-});
+}
