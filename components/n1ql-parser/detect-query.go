60845478bb26bf7a0a482ba5e8418ecce45ce54b
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-03-25 14:01:24 +0530

    Check if query is select query

--- components/n1ql-parser/detect-query.go
+++ components/n1ql-parser/detect-query.go
@@ -8,9 +8,11 @@ import (
 )
 
 func main() {
-	stmt := `INSERT INTO beer VALUES ("a", "b")`
+	//stmt := `INSERT INTO beer VALUES ("a", "b")`
+	stmt := "SELECT t1.country, array_agg(t1.city), sum(t1.city_cnt) as apnum FROM (SELECT city, city_cnt, array_agg(airportname) as apnames, country FROM `travel-sample` WHERE type = $where GROUP BY city, country LETTING city_cnt = count($city) ) AS t1 WHERE t1.city_cnt > 5 GROUP BY t1.country;"
+
 	alg, _ := n1ql.ParseStatement(stmt)
-	fmt.Printf("Got:%%#v %#v\n", alg)
+	/*fmt.Printf("Got:%%#v %#v\n", alg)
 	switch t := alg.(type) {
 	case *algebra.Insert:
 		fmt.Printf("Got:%%#v %#v\n", t)
@@ -25,5 +27,13 @@ func main() {
 		fmt.Printf("Got:%%#v %#v\n", ksp)
 		fmt.Printf("Bucket is %v, bingo!!\n", ksp.Keyspace())
 		break;
+	}*/
+
+	isSelectQuery := false
+	switch alg.(type) {
+	case *algebra.Select:
+		isSelectQuery = true
 	}
+
+	fmt.Printf("isSelectQuery : %v\n", isSelectQuery)
 }
--- components/n1ql-parser/parser-barebones.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/couchbase/query/parser/n1ql"
-
-func main() {
-	q := "select * from `beer-sample` where name == $name"
-	n1ql.JustParse(q)
-}
